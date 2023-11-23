ebfd08b4a2cf3c3fae7a9c58d2036959145fade7
Holtz Yan <yan.holtz.data@gmail.com>
2023-11-23 17:19:42 +0100

    add r project solution

---
+++ rstudio-project/R/analysis.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+# Read data using readr
+data <- readr::read_csv("input/data.csv")
+
+# Summary
+summary(data)
+
+# Calculating mean bill length for different species and islands using dplyr
+data %>%
+  filter(species == "Adelie") %>%
+  group_by(island) %>%
+  summarize(mean_bill_length = round(mean(bill_length_mm, na.rm = TRUE), 2))
+
+# Plot using ggplot2
+data %>%
+  na.omit() %>%
+  ggplot(aes(x = bill_length_mm, y = bill_depth_mm, color = species, shape = species)) +
+  geom_point() +
+  labs(x = "Bill Length (mm)", y = "Bill Depth (mm)", title = "Penguin Bill Dimensions") +
+  scale_shape_manual(values = c("Adelie" = 16, "Chinstrap" = 17, "Gentoo" = 18))
